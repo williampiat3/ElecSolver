d5c8d1e079243fda85c3ea5ed3b8c6adef5c4928
Pimss <44709590+Pimss@users.noreply.github.com>
2025-12-16 13:29:41 +0100

    Import NetlistParser in __init__.py

--- src/ElecSolver/__init__.py
+++ src/ElecSolver/__init__.py
@@ -2,3 +2,4 @@ __version__ = "1.0.4"
 
 from .FrequencySystemBuilder import FrequencySystemBuilder
 from .TemporalSystemBuilder import TemporalSystemBuilder
+from .netlist_parser import NetlistParser
