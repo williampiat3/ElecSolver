cdf31917f69a89bc6c0e05741ab7989c0eb255b3
Pimss <44709590+Pimss@users.noreply.github.com>
2026-03-16 13:30:51 +0100

    Import TemporalNetlistDumper in __init__.py

--- src/ElecSolver/__init__.py
+++ src/ElecSolver/__init__.py
@@ -3,4 +3,4 @@ __version__ = "1.0.6"
 from .FrequencySystemBuilder import FrequencySystemBuilder
 from .TemporalSystemBuilder import TemporalSystemBuilder
 from .NetlistParser import NetlistParser
-
+from .NetlistDumper import TemporalNetlistDumper
